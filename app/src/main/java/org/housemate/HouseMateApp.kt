9f66e92a765b0a6121678b0f2ef05845a7d1dbf6
Nadine Pigida <nadine.pigida@sickkids.ca>
2024-02-12 17:43:08 -0500

    Made some changes to mainactivity

---
+++ app/src/main/java/org/housemate/HouseMateApp.kt
@@ -0,0 +1,8 @@
+package org.housemate
+
+import androidx.compose.runtime.Composable
+@Composable
+fun HouseMateApp() {
+
+
+}
--- app/src/main/java/org/housemate/MainActivity.kt
+++ app/src/main/java/org/housemate/MainActivity.kt
@@ -3,18 +3,12 @@ package org.housemate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import org.housemate.theme.CleandesktopuiTheme
-import org.housemate.model.UserModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val userModel = UserModel()
-
         setContent {
-            CleandesktopuiTheme {
-            }
+            HouseMateApp()
         }
     }
 }
