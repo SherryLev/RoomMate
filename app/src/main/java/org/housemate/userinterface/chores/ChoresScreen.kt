408824d00629cb72a90a90fd07dd75139f65ca2e
Sherry Lev <95260749+SherryLev@users.noreply.github.com>
2024-02-13 16:50:14 -0500

    changes to make the merge work

--- app/src/main/java/org/housemate/userinterface/chores/ChoresScreen.kt
+++ app/src/main/java/org/housemate/userinterface/chores/ChoresScreen.kt
@@ -11,13 +11,14 @@ import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import kotlin.math.sqrt
 data class Task(val name: String)
