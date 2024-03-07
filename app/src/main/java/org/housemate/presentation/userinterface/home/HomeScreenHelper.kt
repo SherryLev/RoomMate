c3c18f8821bba54f035b0f1b173f0b98b75379f8
Nadine Pigida <nadine.pigida@sickkids.ca>
2024-03-07 02:03:33 -0500

    Same commit as last time, didnt upload fully

--- app/src/main/java/org/housemate/presentation/userinterface/home/HomeScreenHelper.kt
+++ app/src/main/java/org/housemate/presentation/userinterface/home/HomeScreenHelper.kt
@@ -1,20 +1,49 @@
 package org.housemate.presentation.userinterface.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun HomeScreenHelper(navController: NavHostController = rememberNavController()) {
+fun HomeScreenHelper(
+    navController: NavHostController = rememberNavController(),
+    onNavigateToSettingsScreen: () -> Unit
+    ) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(text = "Home screen")
+        // Add settings button in the top right corner
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .align(Alignment.TopEnd),
+            horizontalAlignment = Alignment.End
+        ) {
+            IconButton(
+                onClick = {
+                    onNavigateToSettingsScreen()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "Settings"
+                )
+            }
+        }
     }
 }
