c5413dcc678ae63c8c83688c5b4cd75764c6dac8
Nadine Pigida <nadine.pigida@sickkids.ca>
2024-03-07 02:02:46 -0500

    Added settings button and navigation graph to new screen

---
+++ app/src/main/java/org/housemate/presentation/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,14 @@
+package org.housemate.presentation.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(): ViewModel() {
+
+}
