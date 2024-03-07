80a68376c5c02c4bd543342688b0041b30846287
Greg Moroie <gregmoroie@Gregs-MacBook-Pro-2.local>
2024-03-07 16:02:26 -0500

    Created UserRepositoryImpl, user object, user repository, and added to AuthRepositoryImpl

--- app/src/main/java/org/housemate/domain/model/User.kt
+++ app/src/main/java/org/housemate/domain/model/User.kt
@@ -5,5 +5,14 @@ import java.util.*
 
 data class User (
     val email: String = "",
-    val password: String = ""
-)
+    val uId: String = "",
+    val groupCode: String? = null // nullable since when a user joins they are not in a gorup
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "uId" to uId,
+            "email" to email,
+            "groupCode" to groupCode
+        )
+    }
+}
