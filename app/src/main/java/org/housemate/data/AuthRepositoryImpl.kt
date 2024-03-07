abf97615928e7ffae365f3c74630c38a92f8d9e6
Greg Moroie <gregmoroie@Gregs-MacBook-Pro-2.local>
2024-03-07 16:02:26 -0500

    Created UserRepositoryImpl, user object, user repository, and added to AuthRepositoryImpl

--- app/src/main/java/org/housemate/data/AuthRepositoryImpl.kt
+++ app/src/main/java/org/housemate/data/AuthRepositoryImpl.kt
@@ -7,24 +7,40 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.tasks.await
 import org.housemate.domain.repositories.AuthRepository
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class AuthRepositoryImpl : AuthRepository {
     private val firebaseAuth = Firebase.auth
+    private val db = FirebaseFirestore.getInstance()
+
     override suspend fun register(email: String, password: String): Boolean {
         try {
-            firebaseAuth.createUserWithEmailAndPassword(
-                email,
-                password
-            ).await()
-            delay(700)
-            Log.d("main", "User id ${firebaseAuth.currentUser?.uid} created successfully")
-            return true
+            val authResult = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
 
+            authResult.user?.let { firebaseUser ->
+                val userData = hashMapOf(
+                    "uid" to firebaseUser.uid,
+                    "email" to email
+                )
+
+                // Sotre user details in Firestore
+                db.collection("users").document(firebaseUser.uid).set(userData).await()
+                Log.d(
+                    "main",
+                    "User id ${firebaseUser} created successfully and details stored in Firestore"
+                )
+                return true
+            } ?: run {
+                Log.d("main", "User registration succeeded but user data is null.")
+                return false
+            }
         } catch (e: Exception) {
-                Log.d("main", "Failed to register user.")
+                Log.d("main", "Failed to register user or store user details.", e)
                 return false
         }
     }
+
 
     override suspend fun login(email: String, password: String): Boolean {
         try {
