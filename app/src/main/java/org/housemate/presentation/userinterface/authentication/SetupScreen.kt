3c7b061fe40488598ee8b9008515fdc490772f58
Nadine Pigida <nadine.pigida@sickkids.ca>
2024-03-07 19:25:12 -0500

    Fixing name of setupscreen (setupusernamescreen->setupscreen)

--- app/src/main/java/org/housemate/presentation/userinterface/authentication/SetupScreen.kt
+++ app/src/main/java/org/housemate/presentation/userinterface/authentication/SetupScreen.kt
@@ -1,17 +1,9 @@
 package org.housemate.presentation.userinterface.authentication
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -113,7 +104,7 @@ fun MainLayout(navController: NavController) {
 
 
 @Composable
-fun SetupUsernameScreen(navController: NavHostController = rememberNavController()) {
+fun SetupScreen(navController: NavHostController = rememberNavController()) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
